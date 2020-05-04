14ac4fdf9d06744dc96f7421358800c8d6c0fc8a
Matthew Johns <mj@clovenabs.com>
2020-05-04 07:22:04 -0700

    mastering spark chapters 2-3

---
+++ mastering-spark.R
@@ -0,0 +1,117 @@
+
+
+library(sparklyr)
+library(DBI)
+library(dplyr)
+library(sparklyr.nested)
+library(corrr)
+
+sc <- spark_connect(master = "local")
+
+
+#### chapter 2 ####
+
+# read dataset into spark
+
+# cars <- copy_to(sc, mtcars)
+# 
+# 
+# dbGetQuery(sc, "SELECT count(*) FROM mtcars")
+# 
+# 
+# count(cars)
+# 
+# select(cars, hp, mpg) %>% 
+#     sample_n(100) %>% 
+#     collect() %>% 
+#     plot()
+# 
+# 
+# model <- ml_linear_regression(cars, mpg ~ hp)
+# model
+# 
+# 
+# model %>% 
+#     ml_predict(copy_to(sc, data.frame(hp = 250 + 10 * 1:10))) %>% 
+#     transmute(hp = hp, mpg = prediction) %>% 
+#     full_join(select(cars, hp, mpg)) %>% 
+#     collect() %>% 
+#     plot()
+# 
+# 
+# 
+# 
+# sparklyr.nested::sdf_nest(cars, hp) %>% 
+#     group_by(cyl) %>% 
+#     summarise(data = collect_list(data))
+# 
+# 
+# 
+# # streaming
+# 
+# dir.create("input")
+# write.csv(mtcars, "input/cars_1.csv", row.names = F)
+# 
+# stream <- stream_read_csv(sc, "input/") %>% 
+#     select(mpg, cyl, disp) %>% 
+#     stream_write_csv("output/")
+# 
+# write.csv(mtcars, "input/cars_2.csv", row.names = F)
+# 
+# stream_stop(stream)
+# 
+# spark_log(sc)
+# 
+# spark_log(sc, filter = "sparklyr")
+
+
+
+
+#### chapter 3 ####
+
+cars <- copy_to(sc, mtcars)
+
+summarise_all(cars, mean)
+
+
+summarise_all(cars, mean) %>% 
+    show_query()
+
+# percentile using Hive
+
+summarise(cars, mpg_percentile = percentile(mpg, 0.25))
+
+summarise(cars, mpg_percentile = percentile(mpg, 0.25)) %>% 
+    show_query()
+
+
+# array in Hive
+
+summarise(cars, mpg_percentile = percentile(mpg, array(0.25, 0.5, 0.75)))
+
+summarise(cars, mpg_percentile = percentile(mpg, array(0.25, 0.5, 0.75))) %>% 
+    mutate(mpg_percentile = explode(mpg_percentile))
+
+
+
+# correlations
+
+ml_corr(cars)
+
+correlate(cars, use = "pairwise.complete.obs", method = "pearson")
+
+
+correlate(cars, use = "pairwise.complete.obs", method = "pearson") %>% 
+    shave()
+
+
+
+
+
+
+
+
+
+
+
+
